8a08739e291894d05328435522243b812b7e3765
Rafael Haußmann <rafael@lulububu.de>
2023-12-03 17:51:21 +0100

    redesigned options json format and added colors and blur to the options

--- example/index.js
+++ example/index.js
@@ -1,30 +1,66 @@
 const options = [
-  {
-    from: 0,
-    fromCss: {
-      opacity: 0,
-      scale: 0.5,
-      translateY: "-100",
-      rotate: 45,
+    {
+        percentage: {
+            from: 0,
+            to:   90,
+        },
+        styles:     {
+            opacity:    {
+                from: 0,
+                to:   1,
+            },
+            translateY: {
+                from: '-100',
+                to:   '0',
+            },
+            scale:      {
+                from: 0.5,
+                to:   1,
+            },
+            rotate:     {
+                from: 45,
+                to:   0,
+            },
+            blur:       {
+                from: 4,
+                to:   0,
+            },
+            background: {
+                from: '#bb99ff',
+                to:   '#ab6f33',
+            },
+        },
     },
-    to: 30,
-    toCss: {
-      opacity: 1,
-      scale: 1,
-      translateY: "0",
-      rotate: 0,
+    {
+        percentage: {
+            from: 90,
+            to:   100,
+        },
+        styles:     {
+            opacity:    {
+                from: 1,
+                to:   0,
+            },
+            scale:      {
+                from: 1,
+                to:   0.5,
+            },
+            background: {
+                from: '#ab6f33',
+                to:   '#bb99ff',
+            },
+        },
     },
-  },
-  {
-    from: 70,
-    fromCss: {
-      opacity: 1,
-    },
-    to: 100,
-    toCss: {
-      opacity: 0,
-    },
-  },
+
 ];
-initScrollAnimate("container-1", "content-1", options);
-initScrollAnimate("container-2", "content-2", options);
+
+initScrollAnimate(
+    'container-1',
+    'content-1',
+    options,
+);
+initScrollAnimate(
+    'container-2',
+    'content-2',
+    options,
+);
--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const OPTION_STYLES = Object.freeze({
-  OPACITY: "opacity",
-  TRANSLATE_Y: "translateY",
-  TRANSLATE_X: "translateX",
-  SCALE: "scale",
-  ROTATE: "rotate",
+    OPACITY:     'opacity',
+    TRANSLATE_Y: 'translateY',
+    TRANSLATE_X: 'translateX',
+    SCALE:       'scale',
+    ROTATE:      'rotate',
+    COLOR:       'color',
+    BACKGROUND:  'background',
+    BLUR:        'blur',
 });
+
+const NUMERIC_STYLES = Object.freeze([
+    OPTION_STYLES.OPACITY,
+    OPTION_STYLES.SCALE,
+    OPTION_STYLES.ROTATE,
+    OPTION_STYLES.BLUR,
+    OPTION_STYLES.TRANSLATE_Y,
+    OPTION_STYLES.TRANSLATE_X,
+]);
+
+const COLOR_STYLES = Object.freeze([
+    OPTION_STYLES.COLOR,
+    OPTION_STYLES.BACKGROUND,
+]);
 
 /**
  * @param containerId
@@ -22,166 +39,232 @@ const OPTION_STYLES = Object.freeze({
  * const targetId = 'content-1'
  * const options = [
  *    {
- *       from: 0,
- *       fromCss: {
- *       translateY: '-100'
- *    },
- *       to: 30,
- *       toCss: {
- *       translateY: '0'
+ *       percentage: {
+ *          from: 90,
+ *          to:   100,
+ *       },
+ *       styles:     {
+ *          opacity: {
+ *             from: 1,
+ *             to:   0,
+ *          },
+ *          scale:   {
+ *             from: 1,
+ *             to:   0.5,
+ *          },
+ *       },
  *    }
- * }]
+ * ]
  */
-function initScrollAnimate(containerId, targetId, options) {
-  scrollAnimate(containerId, targetId, options);
-
-  document.addEventListener("scroll", () => {
+export function initScrollAnimate(containerId, targetId, options) {
     scrollAnimate(containerId, targetId, options);
-  });
+
+    document.addEventListener('scroll', () => {
+        scrollAnimate(containerId, targetId, options);
+    });
 }
 
 function scrollAnimate(containerId, targetId, options) {
-  const container = document.getElementById(containerId);
-  const target = document.getElementById(targetId);
+    const container = document.getElementById(containerId);
+    const target    = document.getElementById(targetId);
 
-  try {
-    processScrollValues(container, target, options);
-  } catch (e) {
-    console.error(e);
-  }
+    try {
+        processScrollValues(container, target, options);
+    } catch (e) {
+        console.error(e);
+    }
 }
 
 function processScrollValues(container, target, options) {
-  const containerScrollPercentage =
-    calculateContainerScrollPercentage(container);
-  const isContainerVisible =
-    containerScrollPercentage <= 100 && containerScrollPercentage >= 0;
+    const containerScrollPercentage = calculateContainerScrollPercentage(container);
+    const isContainerVisible        = (
+        containerScrollPercentage <= 100 &&
+        containerScrollPercentage >= 0
+    );
 
-  triggerContentVisibility(isContainerVisible, target);
+    triggerContentVisibility(isContainerVisible, target);
 
-  if (!isContainerVisible) {
-    return;
-  }
-
-  options.forEach((option) => {
-    const { from, fromCss, to, toCss } = option;
-    const optionScrollPercentage =
-      (containerScrollPercentage - from) / (to - from);
-    const isScrollOutOfRange =
-      containerScrollPercentage < from || containerScrollPercentage > to;
-
-    if (isScrollOutOfRange) {
-      return;
+    if (!isContainerVisible) {
+        return;
     }
 
-    const cssValues = getCssValues(fromCss, toCss, optionScrollPercentage);
+    options.forEach((option) => {
+        const { from: percentageFrom, to: percentageTo } = option.percentage;
+        // @formatter:off
+        const optionScrollMultiplier =
+                  (containerScrollPercentage - percentageFrom) /
+                  (percentageTo - percentageFrom)
+        ;
+        // @formatter:on
+        const isScrollOutOfRange                         = (
+            containerScrollPercentage < percentageFrom ||
+            containerScrollPercentage > percentageTo
+        );
 
+        if (isScrollOutOfRange) {
+            return;
+        }
+
+        handleCssValues(
+            option,
+            optionScrollMultiplier,
+            target,
+        );
+    });
+}
+
+function handleCssValues(option, optionScrollMultiplier, target) {
+    const { styles } = option;
+
+    let cssValues = [];
+
+    Object.keys(styles).forEach((key) => {
+        if (NUMERIC_STYLES.includes(key)) {
+            handleNumericCssValues(
+                cssValues,
+                key,
+                styles[key],
+                optionScrollMultiplier,
+            );
+        } else if (COLOR_STYLES.includes(key)) {
+            handleColorCssValues(
+                cssValues,
+                key,
+                styles[key],
+                optionScrollMultiplier,
+            );
+        }
+    });
+
+    concatTransformStyles(cssValues);
     appendCssValuesToTarget(cssValues, target);
-  });
+}
+
+function handleColorCssValues(cssValues, key, style, optionScrollMultiplier) {
+    const { from: styleFrom, to: styleTo } = style;
+    const optionScrollPercentage           = optionScrollMultiplier * 100;
+
+    let cssValue  = {};
+    cssValue[key] = `color-mix(in hsl, ${styleFrom}, ${styleTo} ${optionScrollPercentage}%)`;
+
+    cssValues.push(cssValue);
+}
+
+function handleNumericCssValues(cssValues, key, style, optionScrollMultiplier) {
+    const { from: styleFrom, to: styleTo } = style;
+    const cssValue                         = getCssValue(
+        key,
+        styleFrom,
+        styleTo,
+        optionScrollMultiplier,
+    );
+
+    cssValues.push(cssValue);
+
 }
 
 function appendCssValuesToTarget(cssValues, target) {
-  cssValues.forEach((cssValue) => {
-    Object.keys(cssValue).forEach((key) => {
-      target.style[key] = cssValue[key];
+    cssValues.forEach((cssValue) => {
+        Object.keys(cssValue).forEach((key) => {
+            console.log(cssValue[key]);
+            target.style[key] = cssValue[key];
+        });
     });
-  });
 }
 
-function getCssValues(fromCss, toCss, optionScrollPercentage) {
-  let cssValues = [];
+function getCssValue(name, from, to, optionScrollMultiplier) {
+    validateOptionCss(name, from, to);
 
-  Object.keys(fromCss).forEach((key) => {
-    validateOptionCss(key, fromCss, toCss);
+    const fromValue = parseFloat(from);
+    const toValue   = parseFloat(to);
+    const value     = fromValue + (
+        toValue - fromValue
+    ) * optionScrollMultiplier;
 
-    const fromValue = parseFloat(fromCss[key]);
-    const toValue = parseFloat(toCss[key]);
-    const value = fromValue + (toValue - fromValue) * optionScrollPercentage;
+    switch (name) {
+        case OPTION_STYLES.OPACITY:
+            return { opacity: value };
 
-    cssValues.push(getCssValue(key, value));
-  });
+        case OPTION_STYLES.TRANSLATE_Y:
+            return { transform: 'translateY(' + value + 'px)' };
 
-  cssValues = concatTransformStyles(cssValues);
+        case OPTION_STYLES.TRANSLATE_X:
+            return { transform: 'translateX(' + value + 'px)' };
 
-  return cssValues;
+        case OPTION_STYLES.SCALE:
+            return { transform: 'scale(' + value + ')' };
+
+        case OPTION_STYLES.ROTATE:
+            return { transform: 'rotate(' + value + 'deg)' };
+
+        case OPTION_STYLES.BLUR:
+            return { filter: 'blur(' + value + 'px)' };
+    }
 }
 
 function concatTransformStyles(cssValues) {
-  let transformString = "";
+    let transformString = '';
 
-  cssValues = cssValues.filter((style) => {
-    const transformValue = style["transform"];
+    cssValues = cssValues.filter((style) => {
+        const transformValue = style['transform'];
 
-    if (!transformValue) {
-      return true;
+        if (!transformValue) {
+            return true;
+        }
+
+        transformString += `${transformValue} `;
+        return false;
+    });
+
+    cssValues.push({
+        transform: transformString,
+    });
+}
+
+function validateOptionCss(key, from, to) {
+    const allowedKeys  = Object.values(OPTION_STYLES);
+    const isKeyAllowed = allowedKeys.includes(key);
+
+    if (!isKeyAllowed) {
+        throw new Error(`Key ${key} is not allowed`);
     }
 
-    transformString += `${transformValue} `;
-    return false;
-  });
-
-  cssValues.push({
-    transform: transformString,
-  });
-
-  return cssValues;
-}
-
-function getCssValue(key, value) {
-  switch (key) {
-    case OPTION_STYLES.OPACITY:
-      return { opacity: value };
-
-    case OPTION_STYLES.TRANSLATE_Y:
-      return { transform: "translateY(" + value + "px)" };
-
-    case OPTION_STYLES.TRANSLATE_X:
-      return { transform: "translateX(" + value + "px)" };
-
-    case OPTION_STYLES.SCALE:
-      return { transform: "scale(" + value + ")" };
-
-    case OPTION_STYLES.ROTATE:
-      return { transform: "rotate(" + value + "deg)" };
-  }
-}
-
-function validateOptionCss(key, fromCss, toCss) {
-  const allowedKeys = Object.values(OPTION_STYLES);
-  const isKeyAllowed = allowedKeys.includes(key);
-
-  if (!isKeyAllowed) {
-    throw new Error(`Key ${key} is not allowed`);
-  }
-
-  const isKeyInToCss = key in toCss;
-  if (!isKeyInToCss) {
-    throw new Error(`Key ${key} is not in toCss`);
-  }
+    if (
+        isNullOrUndefined(from) ||
+        isNullOrUndefined(to)
+    ) {
+        throw new Error(`Key ${key} needs a from and a to value`);
+    }
 }
 
 function triggerContentVisibility(isContainerVisible, target) {
-  if (isContainerVisible) {
-    target.style = "visibility: visible";
-  } else {
-    target.style = "visibility: hidden";
-  }
+    if (isContainerVisible) {
+        target.style = 'visibility: visible';
+    } else {
+        target.style = 'visibility: hidden';
+    }
 }
 
 function calculateContainerScrollPercentage(container) {
-  const containerHeight = container.offsetHeight;
-  const containerOffsetTop = container.offsetTop;
-  const isFirstContainer = containerOffsetTop === 0;
-  let currentTotalScroll = window.pageYOffset + window.innerHeight;
+    const containerHeight    = container.offsetHeight;
+    const containerOffsetTop = container.offsetTop;
+    const isFirstContainer   = containerOffsetTop === 0;
+    let currentTotalScroll   = window.pageYOffset + window.innerHeight;
 
-  if (isFirstContainer) {
-    currentTotalScroll -= window.innerHeight;
-  }
+    if (isFirstContainer) {
+        currentTotalScroll -= window.innerHeight;
+    }
 
-  const containerStartScroll = currentTotalScroll - containerOffsetTop;
-  const containerMaxScroll = isFirstContainer
-    ? containerHeight - window.innerHeight
-    : containerHeight;
+    const containerStartScroll = currentTotalScroll - containerOffsetTop;
+    const containerMaxScroll   = isFirstContainer
+        ? containerHeight - window.innerHeight
+        : containerHeight;
 
-  return (containerStartScroll / containerMaxScroll) * 100;
+    return (
+        containerStartScroll / containerMaxScroll
+    ) * 100;
+}
+
+function isNullOrUndefined(value) {
+    return value === null || value === undefined;
 }
